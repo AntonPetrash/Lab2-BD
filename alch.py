eb7696535ecc5278360cf30b5d34aecc38490b92
Anton Petrash <petrashanton04@gmail.com>
2023-12-24 20:23:48 +0200

    Final commit

--- alch.py
+++ alch.py
@@ -2,12 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Connecting to the database
-DATABASE_URL = 'postgresql://postgres:1@localhost:5432/postgres'
+DATABASE_URL = 'postgresql://postgres:qwerty@localhost:5432/postgres'
 engine = create_engine(DATABASE_URL)
 
-# Creating a base class from which all models will inherit
 Base = declarative_base()
 
-# Create a session to interact with the database
 Session = sessionmaker()
